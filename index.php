c099273cb053c04a527cffc4613e981ed45f7845
Davide Buccella <buccella.davide@gmail.com>
2022-01-09 00:32:13 +0100

    Inserito un messaggio promemoria per le pagine da abilitare sull'htaccess

--- index.php
+++ index.php
@@ -1,57 +1,35 @@
 <?php
-    session_start();
-    // include configurazione base
-    include("script/config.php");
-    
-    // apri database
-    include("script/apridb.php");
+session_start();
+// include configurazione base
+include "script/config.php";
 
-    //echo date("d-m-Y", strtotime("2018-09-20 15:57:45"))."\n"; 
+// apri database
+include "script/apridb.php";
 
-    //livello dell'utente
-    $livello = isset($_SESSION["livello"]) ? $_SESSION["livello"] : "100";
-    $id_operatore = isset($_SESSION["id_operatore"]) ? $_SESSION["id_operatore"] : "no";
+//echo date("d-m-Y", strtotime("2018-09-20 15:57:45"))."\n";
 
+//livello dell'utente
+$livello = isset($_SESSION["livello"]) ? $_SESSION["livello"] : "100";
+$id_operatore = isset($_SESSION["id_operatore"]) ? $_SESSION["id_operatore"] : "no";
 
-    // apri pagina
-    $page = (isset($_GET["page"])) ? $_GET["page"] : "Home"; 
-    $dir = (isset($_GET["ris"])) ? "AreaRiservata" : "pagine"; 
+// apri pagina
+$page = (isset($_GET["page"])) ? $_GET["page"] : "Home";
+$dir = (isset($_GET["ris"])) ? "AreaRiservata" : "pagine";
 
+//include("script/apridb.php");
+include "template/header.php";
 
-    //include("script/apridb.php");
-    include("template/header.php");
+// inizio body
+include "template/menu.php";
 
-    // inizio body
-    include("template/menu.php");
-    
+function exception_error_handler($errno, $errstr, $errfile, $errline ) {
+    throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
+}
+set_error_handler("exception_error_handler");
 
-    // caricamento corpo della pagina.
-    include("$dir/$page.php");
+// caricamento corpo della pagina.
+if (file_exists("$dir/$page.php")) include "$dir/$page.php";
+else echo '<h1 style="color: red; padding: 5rem;">Contattare l\'amministratore del sito per abilitare la pagina.</h1>';
 
-
-
-
-    include("template/footer.php"); 
-    $conn->close();
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+include "template/footer.php";
+$conn->close();
