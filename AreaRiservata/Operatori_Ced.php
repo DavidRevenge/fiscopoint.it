5ff0b911ff22fb6a091d187a7d799f9834fde254
Davide Buccella <buccella.davide@gmail.com>
2022-01-09 00:47:29 +0100

    fix

--- AreaRiservata/Operatori_Ced.php
+++ AreaRiservata/Operatori_Ced.php
@@ -1,29 +1,29 @@
-<?php 
-    if($livello != 0) {
-        echo "<script type=\"text/javascript\">window.location.replace(\"$sito\");</script>";
-        return;
-    } 
-    // titolo della pagina
-    $titolo_pagina = "Operatori CED";
-    include("template/titolo_pagina.php");
+<?php
+if ($livello != 0) {
+    echo "<script type=\"text/javascript\">window.location.replace(\"$sito\");</script>";
+    return;
+}
+// titolo della pagina
+$titolo_pagina = "Operatori CED";
+include "template/titolo_pagina.php";
 
-    $livelli_json = json_decode(file_get_contents("json/livelli.json"), true);
+$livelli_json = json_decode(file_get_contents("json/livelli.json"), true);
 
-    if(isset($_GET["oper"])) {
-        $sql = "UPDATE operatori SET Stato = NOT Stato WHERE indice=?";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param('i', $_GET["id"]);
-        $stmt->execute();
-    }
+if (isset($_GET["oper"])) {
+    $sql = "UPDATE operatori SET Stato = NOT Stato WHERE indice=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('i', $_GET["id"]);
+    $stmt->execute();
+}
 ?>
-<div class="card p-2 m-2">  
+<div class="card p-2 m-2">
     <div class="card-body">
         <div class="row">
             <div class="col-md-3">
                 <a class="btn btn-primary" href="<?php echo $sito ?>Area-Riservata/Aggiungi-Operatore-ced.html">Aggiungi operatore CED</a>
             </div>
             <div class="col-md-9">
-                <form action="<?php echo $sito ?>Area-Riservata/Operatori_ced.html" method="POST"> 
+                <form action="<?php echo $sito ?>Area-Riservata/Operatori_ced.html" method="POST">
                     <div class="row justify-content-end">
                         <div class="col-auto">
                             <label for="cerca-operatore-ced" class="col-form-label">Cerca Operatore CED</label>
@@ -36,10 +36,10 @@
                             <button type="submit" class="btn btn-primary">Cerca</button>
                         </div>
                     </div>
-                </form>    
+                </form>
             </div>
         </div>
-    </div> 
+    </div>
 </div>
 
 
@@ -60,40 +60,39 @@
                     <th scope="col">Azione</th>
                 </tr>
             </thead>
-            <tbody>   
-        <?php   
+            <tbody>
+        <?php
 
-        
-        $cond = isset($_POST["cerca"]) ? $_POST["testo"] : "";      
-        $sql = "SELECT * FROM `operatori_ced` WHERE (Username like CONCAT('%', ?, '%') OR Nome like CONCAT('%', ?, '%') OR Cognome like CONCAT('%', ?, '%')) AND id > 1";
-        $stmt = $conn->prepare($sql);  
-        $stmt->bind_param("sss", $cond, $cond, $cond);    
-        $stmt->execute();
-        $result = $stmt->get_result(); 
+$cond = isset($_POST["cerca"]) ? $_POST["testo"] : "";
+// $sql = "SELECT * FROM operatori_ced WHERE (Username like CONCAT('%', ?, '%') OR Nome like CONCAT('%', ?, '%') OR Cognome like CONCAT('%', ?, '%')) AND id > 1";
+$sql = "SELECT * FROM operatori_ced WHERE (Username like CONCAT('%', ?, '%') OR Nome like CONCAT('%', ?, '%') OR Cognome like CONCAT('%', ?, '%'))";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sss", $cond, $cond, $cond);
+$stmt->execute();
+$result = $stmt->get_result();
 
+while ($row = $result->fetch_assoc()) {
+    $id = $row["id"];
+    $username = $row["Username"];
+    $nome = $row["Nome"];
+    $cognome = $row["Cognome"];
+    $email = $row["Email"];
+    $liv = $livelli_json[$row["Livello"]]["descrizione"];
+    // $stato = $row["Stato"];
 
-        while ($row = $result->fetch_assoc()){
-            $id = $row["indice"];
-            $username = $row["Username"];
-            $nome = $row["Nome"];
-            $cognome = $row["Cognome"];
-            $email = $row["Email"];
-            $liv = $livelli_json[$row["Livello"]]["descrizione"];
-            $stato = $row["Stato"];
-            
-
-            $status = ($stato) ? "<a href=\"{$sito}Area-Riservata/Operatori-$id.html\" class=\"btn btn-primary\">Disabilita</a>" : "<a href=\"{$sito}Area-Riservata/Operatori-$id.html\" class=\"btn btn-primary\">Abilita</a>";
-            $status .= "<a class=\"ms-2 btn btn-primary lista_operatori\" href=\"{$sito}Area-Riservata/Modifica-Operatore-$id.html\">Modifica</a>";
-            $row_color = (!$stato) ? "bg-danger text-white" : "";
-            echo "<tr class=\"$row_color\" > 
+    // $status = ($stato) ? "<a href=\"{$sito}Area-Riservata/Operatori-$id.html\" class=\"btn btn-primary\">Disabilita</a>" : "<a href=\"{$sito}Area-Riservata/Operatori-$id.html\" class=\"btn btn-primary\">Abilita</a>";
+    $status = "<a class=\"ms-2 btn btn-primary lista_operatori\" href=\"{$sito}Area-Riservata/Modifica-Operatore-$id.html\">Modifica</a>";
+    // $row_color = (!$stato) ? "bg-danger text-white" : "";
+    // echo "<tr class=\"$row_color\" >
+    echo "<tr >
                     <td>$username</td>
                     <td class=\"\">$nome $cognome</td>
                     <td>$email</td>
                     <td>$liv</td>
                     <td>$status</td>
                 </tr>";
-        }
-        ?>
+}
+?>
 
             </tbody>
         </table>
