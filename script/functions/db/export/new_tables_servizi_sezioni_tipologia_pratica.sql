ab8fbeebd4c6d1f6896a029bdabe640172f5d596
Enrico217 <enrico217@gmail.com>
2022-01-18 13:17:08 +0100

    Update new_tables_servizi_sezioni_tipologia_pratica.sql
    correzione file sql

--- script/functions/db/export/new_tables_servizi_sezioni_tipologia_pratica.sql
+++ script/functions/db/export/new_tables_servizi_sezioni_tipologia_pratica.sql
@@ -16,76 +16,76 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `servizi`
+-- Table structure for table `fiscopoint`.`servizi`
 --
 
-DROP TABLE IF EXISTS `servizi`;
+DROP TABLE IF EXISTS `fiscopoint`.`servizi`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `servizi` (
+CREATE TABLE `fiscopoint`.`servizi` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `servizi`
+-- Dumping data for table `fiscopoint`.`servizi`
 --
 
-LOCK TABLES `servizi` WRITE;
-/*!40000 ALTER TABLE `servizi` DISABLE KEYS */;
-INSERT INTO `servizi` VALUES (1,'CAF - Assistenza Fiscale'),(2,'Servizio di Patronato'),(3,'Gestione Contabilità e Paghe'),(4,'Colf e Badanti'),(5,' Pratiche telematiche'),(6,'Fatturazione Elettronica'),(7,' Formazione'),(8,' Privacy'),(9,' Assicurazioni'),(10,' Accesso al Credito'),(11,' Moneta Elettronica'),(12,'Centro raccolta CAF');
-/*!40000 ALTER TABLE `servizi` ENABLE KEYS */;
+LOCK TABLES `fiscopoint`.`servizi` WRITE;
+/*!40000 ALTER TABLE `fiscopoint`.`servizi` DISABLE KEYS */;
+INSERT INTO `fiscopoint`.`servizi` VALUES (1,'CAF - Assistenza Fiscale'),(2,'Servizio di Patronato'),(3,'Gestione Contabilità e Paghe'),(4,'Colf e Badanti'),(5,' Pratiche telematiche'),(6,'Fatturazione Elettronica'),(7,' Formazione'),(8,' Privacy'),(9,' Assicurazioni'),(10,' Accesso al Credito'),(11,' Moneta Elettronica'),(12,'Centro raccolta CAF');
+/*!40000 ALTER TABLE `fiscopoint`.`servizi` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `sezioni`
+-- Table structure for table `fiscopoint`.`sezioni`
 --
 
-DROP TABLE IF EXISTS `sezioni`;
+DROP TABLE IF EXISTS `fiscopoint`.`sezioni`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `sezioni` (
+CREATE TABLE `fiscopoint`.`sezioni` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `sezioni`
+-- Dumping data for table `fiscopoint`.`sezioni`
 --
 
-LOCK TABLES `sezioni` WRITE;
-/*!40000 ALTER TABLE `sezioni` DISABLE KEYS */;
-INSERT INTO `sezioni` VALUES (0,'CAF'),(1,'Altre Pratiche'),(2,'Assicurazioni');
-/*!40000 ALTER TABLE `sezioni` ENABLE KEYS */;
+LOCK TABLES `fiscopoint`.`sezioni` WRITE;
+/*!40000 ALTER TABLE `fiscopoint`.`sezioni` DISABLE KEYS */;
+INSERT INTO `fiscopoint`.`sezioni` VALUES (0,'CAF'),(1,'Altre Pratiche'),(2,'Assicurazioni');
+/*!40000 ALTER TABLE `fiscopoint`.`sezioni` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Table structure for table `tipologia_pratica`
 --
 
-DROP TABLE IF EXISTS `tipologia_pratica`;
+DROP TABLE IF EXISTS `fiscopoint`.`tipologia_pratica`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tipologia_pratica` (
+CREATE TABLE `fiscopoint`.`tipologia_pratica` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) DEFAULT NULL,
   `id_sezione` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `tipologia_pratica`
+-- Dumping data for table `fiscopoint`.`tipologia_pratica`
 --
 
-LOCK TABLES `tipologia_pratica` WRITE;
-/*!40000 ALTER TABLE `tipologia_pratica` DISABLE KEYS */;
-INSERT INTO `tipologia_pratica` VALUES (1,'MOD. 730',0),(2,'ISEE',0),(3,'RED',0),(4,'MOD. ICLAV',0),(5,'MOD. ICRIC',0),(6,'MOD. PSAS/ACC',0),(7,'UNICO PF',0),(8,'IMU',0),(9,'ALTRO',0),(10,'PATRONATO',1),(11,'SUCCESSIONI',1),(12,'CONTRATTI DI LOCAZIONE',1),(13,'VISURE CATASTALI',1),(14,'VISURE CAMERALI',1),(15,'VISURE CONSERVATORIA IMM.',1),(16,'FORMAZIONE',1),(17,'MONETA ELETTRONICA',1),(18,'PRIVACY',1),(19,'PERMESSI RINNOVI DI SOGGIORNO',1),(20,'ASSICURAZIONI',2);
-/*!40000 ALTER TABLE `tipologia_pratica` ENABLE KEYS */;
+LOCK TABLES `fiscopoint`.`tipologia_pratica` WRITE;
+/*!40000 ALTER TABLE `fiscopoint`.`tipologia_pratica` DISABLE KEYS */;
+INSERT INTO `fiscopoint`.`tipologia_pratica` VALUES (1,'MOD. 730',0),(2,'ISEE',0),(3,'RED',0),(4,'MOD. ICLAV',0),(5,'MOD. ICRIC',0),(6,'MOD. PSAS/ACC',0),(7,'UNICO PF',0),(8,'IMU',0),(9,'ALTRO',0),(10,'PATRONATO',1),(11,'SUCCESSIONI',1),(12,'CONTRATTI DI LOCAZIONE',1),(13,'VISURE CATASTALI',1),(14,'VISURE CAMERALI',1),(15,'VISURE CONSERVATORIA IMM.',1),(16,'FORMAZIONE',1),(17,'MONETA ELETTRONICA',1),(18,'PRIVACY',1),(19,'PERMESSI RINNOVI DI SOGGIORNO',1),(20,'ASSICURAZIONI',2);
+/*!40000 ALTER TABLE `fiscopoint`.`tipologia_pratica` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
